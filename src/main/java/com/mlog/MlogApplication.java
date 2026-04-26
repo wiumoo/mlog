2977a818c6f85f51ad61345d8323893a0977db30
wiumoo <parkminwoo0402@naver.com>
2026-04-27 00:21:34 +0800

    main

---
+++ src/main/java/com/mlog/MlogApplication.java
@@ -0,0 +1,13 @@
+package com.mlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mlog/MlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
